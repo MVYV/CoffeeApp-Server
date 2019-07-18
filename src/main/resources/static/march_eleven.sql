97442a18dadd879645d9ad48a0132ca524657a67
yuriyvalkiv <yurivalkiv@gmail.com>
2019-07-18 20:04:30 +0200

    sql database

--- src/main/resources/static/march_eleven.sql
+++ src/main/resources/static/march_eleven.sql
@@ -2,10 +2,10 @@
 -- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Час створення: Трв 29 2019 р., 16:55
--- Версія сервера: 5.7.24
--- Версія PHP: 7.2.14
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 18, 2019 at 06:03 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База даних: `march_eleven`
+-- Database: `march_eleven`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `hibernate_sequence`
+-- Table structure for table `hibernate_sequence`
 --
 
 DROP TABLE IF EXISTS `hibernate_sequence`;
@@ -34,17 +34,17 @@ CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Дамп даних таблиці `hibernate_sequence`
+-- Dumping data for table `hibernate_sequence`
 --
 
 INSERT INTO `hibernate_sequence` (`next_val`) VALUES
-(7),
-(7);
+(12),
+(12);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `news`
+-- Table structure for table `news`
 --
 
 DROP TABLE IF EXISTS `news`;
@@ -53,20 +53,44 @@ CREATE TABLE IF NOT EXISTS `news` (
   `title` varchar(60) NOT NULL,
   `sub_text` varchar(150) NOT NULL,
   `text` text NOT NULL,
+  `source` varchar(100) DEFAULT NULL,
+  `created_on` timestamp NULL DEFAULT NULL,
+  `modified_on` timestamp NULL DEFAULT NULL,
+  `image` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`news_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
--- Дамп даних таблиці `news`
+-- Dumping data for table `news`
 --
 
-INSERT INTO `news` (`news_id`, `title`, `sub_text`, `text`) VALUES
-(1, 'Chelsea\'s loan players', 'We round up news involving our loan players following a play-off in the Netherlands and mixed fortunes from the Italian top flight…\r\n', 'Having finished fifth in Eredivisie table, Vitesse were involved in a play-off with Utrecht for a place in next season’s Europa League. They were unable to select Jake Clarke-Salter for last week’s first leg due to a knee problem, the on-loan Chelsea defender having been a mainstay of the Vitesse defence this season, and longer-term casualty Charly Musonda was unavailable again having returned to the pitch earlier in the month.\r\n\r\nThat game away at Utrecht ended 1-1 so hopes must have been high at Vitesse ahead of Tuesday’s home leg, but their visitors ran out 2-0 winners on the night. Again Clarke-Salter could not play.\r\n\r\nThe Serie A season concluded at the weekend and Mario Pasalic (pictured top centre) was on the scoresheet, netting the third in Atalanta’s 3-1 home win over Sassuolo which meant the Bergamo side ended the season in a very impressive third place behind Juventus and Napoli, earning them a place in the Champions League for the first time in their history.');
+INSERT INTO `news` (`news_id`, `title`, `sub_text`, `text`, `source`, `created_on`, `modified_on`, `image`) VALUES
+(1, 'Chelsea\'s loan players', 'We round up news involving our loan players following a play-off in the Netherlands and mixed fortunes from the Italian top flight…\r\n', 'Having finished fifth in Eredivisie table, Vitesse were involved in a play-off with Utrecht for a place in next season’s Europa League. They were unable to select Jake Clarke-Salter for last week’s first leg due to a knee problem, the on-loan Chelsea defender having been a mainstay of the Vitesse defence this season, and longer-term casualty Charly Musonda was unavailable again having returned to the pitch earlier in the month.\r\n\r\nThat game away at Utrecht ended 1-1 so hopes must have been high at Vitesse ahead of Tuesday’s home leg, but their visitors ran out 2-0 winners on the night. Again Clarke-Salter could not play.\r\n\r\nThe Serie A season concluded at the weekend and Mario Pasalic (pictured top centre) was on the scoresheet, netting the third in Atalanta’s 3-1 home win over Sassuolo which meant the Bergamo side ended the season in a very impressive third place behind Juventus and Napoli, earning them a place in the Champions League for the first time in their history.', NULL, NULL, '2019-06-19 06:12:01', NULL),
+(11, 'title', 'sub', 'text', 'asds', '2019-06-25 15:24:18', '2019-06-19 06:12:01', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `roles`
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `image` varchar(256) DEFAULT NULL,
+  `description` varchar(256) NOT NULL,
+  `price` double NOT NULL,
+  `created_on` timestamp NOT NULL,
+  `modified_on` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
 --
 
 DROP TABLE IF EXISTS `roles`;
@@ -77,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `roles` (
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Дамп даних таблиці `roles`
+-- Dumping data for table `roles`
 --
 
 INSERT INTO `roles` (`role_id`, `role`) VALUES
@@ -89,7 +113,7 @@ INSERT INTO `roles` (`role_id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `users`
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -100,21 +124,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(30) DEFAULT NULL,
   `last_name` varchar(30) DEFAULT NULL,
   `active` tinyint(4) NOT NULL,
+  `avatar` varchar(256) DEFAULT NULL,
+  `country` varchar(30) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `birth` timestamp NULL DEFAULT NULL,
+  `gender` varchar(6) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
--- Дамп даних таблиці `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `email`, `password`, `name`, `last_name`, `active`) VALUES
-(1, 'yura@gmail.com', '$2a$10$2mkMpMK.p/GMiPMjiUb4ROAQpRrnEuMCn8Kp6DPU1XFEIa2lsRlhS', 'yura', 'yura', 1),
-(24, 'palmer@mail.com', '$2a$10$iriuqrDPPxu1HoTufSf8wei16k32iaW36mCmaL2sI31zCUelPhpO2', 'Meredith', 'Without nothing3', 1);
+INSERT INTO `users` (`user_id`, `email`, `password`, `name`, `last_name`, `active`, `avatar`, `country`, `city`, `birth`, `gender`) VALUES
+(1, 'yura@gmail.com', '$2a$10$2mkMpMK.p/GMiPMjiUb4ROAQpRrnEuMCn8Kp6DPU1XFEIa2lsRlhS', 'yura', 'yura', 1, NULL, NULL, NULL, '2019-07-09 22:00:00', NULL),
+(24, 'palmer@mail.com', '$2a$10$iriuqrDPPxu1HoTufSf8wei16k32iaW36mCmaL2sI31zCUelPhpO2', 'Meredith', 'Without nothing3', 1, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблиці `user_role`
+-- Table structure for table `user_role`
 --
 
 DROP TABLE IF EXISTS `user_role`;
@@ -126,7 +155,7 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Дамп даних таблиці `user_role`
+-- Dumping data for table `user_role`
 --
 
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
