cfe16cc989768e7c66abf5a7850fdae7c778de07
yuriyvalkiv <yurivalkiv@gmail.com>
2019-10-11 09:54:44 +0200

    security update 3

--- src/main/java/com/mvyv/march11webapp/config/SpringSecurityConfig.java
+++ src/main/java/com/mvyv/march11webapp/config/SpringSecurityConfig.java
@@ -62,15 +62,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     httpSecurity.csrf().disable()
             // dont authenticate this particular request
             .authorizeRequests().antMatchers("/authenticate").
-            permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
-            .permitAll()
-            .anyRequest().permitAll();
+            permitAll().antMatchers("/**")
+            .permitAll().
             // all other requests need to be authenticated
-//                    anyRequest().authenticated().and().
+                    anyRequest().authenticated().and().
             // make sure we use stateless session; session won't be used to
             // store user's state.
-//                    exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     // Add a filter to validate the tokens with every request
     httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
