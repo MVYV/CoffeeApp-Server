f5d210c021f6e6584e7a50af977a8be5cee5dfb2
yuriyvalkiv <yurivalkiv@gmail.com>
2019-10-11 09:49:29 +0200

    security update 2

--- src/main/java/com/mvyv/march11webapp/config/SpringSecurityConfig.java
+++ src/main/java/com/mvyv/march11webapp/config/SpringSecurityConfig.java
@@ -63,7 +63,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
             // dont authenticate this particular request
             .authorizeRequests().antMatchers("/authenticate").
             permitAll().antMatchers(HttpMethod.OPTIONS, "/**")
-            .permitAll();
+            .permitAll()
+            .anyRequest().permitAll();
             // all other requests need to be authenticated
 //                    anyRequest().authenticated().and().
             // make sure we use stateless session; session won't be used to
