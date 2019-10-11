5f587df4e64865ec5ac2db86d1c9d434f026e468
yuriyvalkiv <yurivalkiv@gmail.com>
2019-10-11 17:43:27 +0200

    jwt update 2

--- src/main/java/com/mvyv/march11webapp/config/SpringSecurityConfig.java
+++ src/main/java/com/mvyv/march11webapp/config/SpringSecurityConfig.java
@@ -62,8 +62,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     httpSecurity.csrf().disable()
             // dont authenticate this particular request
             .authorizeRequests().antMatchers("/authenticate").
-            permitAll().antMatchers("/")
-            .permitAll().antMatchers("/news").permitAll().
+            permitAll().antMatchers("/").permitAll()
+            .antMatchers("/news").permitAll()
+            .antMatchers("/product").permitAll().
             // all other requests need to be authenticated
                     anyRequest().authenticated().and().
             // make sure we use stateless session; session won't be used to
