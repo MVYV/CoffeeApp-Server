6d62f9abb0cf765bae2ae300da993826a32fc75e
yuriyvalkiv <yurivalkiv@gmail.com>
2019-10-15 14:27:13 +0200

    updated getUserByEmail repository

--- src/main/java/com/mvyv/march11webapp/repository/UserRepository.java
+++ src/main/java/com/mvyv/march11webapp/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.mvyv.march11webapp.repository;
 
 import com.mvyv.march11webapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-  User findByEmail(String email);
+  User findByEmail(@Param("email") String email);
 }
