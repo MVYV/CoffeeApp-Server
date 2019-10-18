ce8490bbd2f4152de8bb33930fea4ff1feec8f83
yuriyvalkiv <yurivalkiv@gmail.com>
2019-10-18 12:23:29 +0200

    comment path change

--- src/main/java/com/mvyv/march11webapp/controller/CommentController.java
+++ src/main/java/com/mvyv/march11webapp/controller/CommentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("comment")
+@RequestMapping("/comment")
 public class CommentController {
 
   private final CommentService commentService;
